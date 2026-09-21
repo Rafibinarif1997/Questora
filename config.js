b4d170cb206b1ad6579db0ea2f4e3978f6160fcb
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-09-21 12:40:45 +0600

    Add REOWN_PROJECT_ID to config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const SUPABASE_URL = "https://uyuzrcavyehcbsekgsmc.supabase.co";
 const SUPABASE_KEY = "sb_publishable_nu1dgJhpsiF7aMaXPkf_JA_Vy25aUC9";
+const REOWN_PROJECT_ID = "ebf0ea15cccd40ba0abab4282d0310e4";
