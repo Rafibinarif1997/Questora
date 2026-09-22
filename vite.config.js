1090f1873483f1eba08d50ea3efb3795e4176d02
Rafibinarif1997 <98096402+Rafibinarif1997@users.noreply.github.com>
2026-09-22 18:22:19 +0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  // Relative assets make the app work on GitHub Pages project URLs.
-  base: './',
-})
